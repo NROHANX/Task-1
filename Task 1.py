a8be3146648bbab9dc03466be0464ee0841525b3
NROHANX <nr007@ymail.com>
2025-08-09 16:27:08 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,18 @@
+a=input('Enter the First Number: ')
+b=input('Enter the Second Number: ')
+Addition=5
+Addition+=10
+print('Addition', Addition)
+Subtraction=5
+Subtraction-=10
+print('Subtraction', Subtraction)
+Multiplication=5
+Multiplication*=10
+print('Multiplication', Multiplication)
+Division=5
+Division/=10
+print('Division', Division)
+
+
+
+
---
+++ Task 2.py
@@ -0,0 +1,6 @@
+a=input('Enter your first name: ')
+b=input('Enter your last name: ')
+#Hello, John Doe! Welcome to the Python Program
+print('Hello, John Doe! Welcome to the Python Program')
+
+
