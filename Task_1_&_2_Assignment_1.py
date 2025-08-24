433a375eea4a792f24b412f0428eef0d2c91bb3d
NROHANX <nr007@ymail.com>
2025-08-24 18:59:48 +0530

    Task_1_&_2_Assignment_1.py

---
+++ Task_1_&_2_Assignment_1.py
@@ -0,0 +1,21 @@
+# Task_1_Assignment_1
+a=input('Enter the First Number: ')
+b=input('Enter the Second Number: ')
+a=5
+a+=10
+print(a)
+a=5
+a-=10
+print(a)
+a=5
+a*=10
+print(a)
+a=5
+a/=10
+print(a)
+
+
+# Task_2_Assignment_2
+a=input('Enter your first name: ')
+b=input('Enter your last name: ')
+print('Hello, John Doe! Welcome to the Python Program')
