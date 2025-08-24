b81968137f28fedbda463b0bbce877bf7efa7fe8
NROHANX <nr007@ymail.com>
2025-08-24 19:03:09 +0530

    Task_1_&_2_Assignment_2.py

---
+++ Task_1_&_2_Assignment_2.py
@@ -0,0 +1,13 @@
+# Task_1_Assignment_2
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
+
+
+# Task_2_Assignment_2
+Total=0
+for i in range(1,51):
+    Total+=i
+print("The sum of numbers from 1 to 50 is:", Total)
